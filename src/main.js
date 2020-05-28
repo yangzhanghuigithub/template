cfa9135a0a0420a5acaaf03fd657123ecf11bdff
yzh <m15711383458@163.com>
2020-05-28 23:20:14 +0800

    提交

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import store from './store'
+import reques from './utils/httpRequest'
 import iview from 'iview';
 import 'iview/dist/styles/iview.css'
 import cascaderMulti from 'cascader-multi';
