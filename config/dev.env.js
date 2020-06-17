5183789844a852d93ca775c6475075fcea705d88
miwentao <miwentao@lrhealth.com>
2020-06-17 18:37:45 +0800

    modify base url for test

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_API: '"http://localhost:8765"'
+  // BASE_API: '"http://localhost:8765"'
+  BASE_API: '"http://hvea83.natappfree.cc"'
 })
