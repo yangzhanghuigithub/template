faf178cae86379f249fb1999c21ea2970a311449
yzh <m15711383458@163.com>
2020-07-10 23:37:45 +0800

    提交

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="page">
     <div class="login-box">
-      <div style="font-size: 20px;margin-right: 214px;margin-bottom: 30px">用户登录:</div>
+      <div style="font-size: 20px;margin-right: 214px;margin-bottom: 30px">用户登录:</div><div @click="toRegist">免费注册</div>
       <el-input placeholder="请输入用户名" prefix-icon="el-icon-user" @input="change($event)" v-model="user.username"
                 style="width: 300px;"></el-input>
       <el-input placeholder="请输入密码" prefix-icon="el-icon-user-solid" @input="change($event)" show-password
                 v-model="user.password" style="width: 300px;margin-top: 30px"></el-input>
       <i-button type="success" style="width: 300px;margin-top: 30px" @click="toLogin()">登录</i-button>
+      <div @click="toReset">忘记密码</div>
       <span style="display: flex;flex-direction: row;width: 24%;justify-content: space-around;margin-top: 30px">
         <span style="line-height: 70px;font-size: 18px">第三方登录:</span>
         <i style="color: skyblue;font-size: 45px" class="third-icon iconfont iconlogo-qq"></i>
@@ -33,6 +34,12 @@
     methods: {
       toIndex() {
         this.$router.push("/index");
+      },
+      toRegist(){
+        this.$router.push("/regist");
+      },
+      toReset(){
+        this.$router.push("/reset");
       },
       change(e) {
         this.$forceUpdate();
